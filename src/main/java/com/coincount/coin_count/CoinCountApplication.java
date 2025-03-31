e00dbf236c464134b94f6449dcd44fdd5d59a093
GDMontsho <montshogoitseone@gmail.com>
2025-03-31 12:27:36 +0200

    init commit: set up dev env

---
+++ src/main/java/com/coincount/coin_count/CoinCountApplication.java
@@ -0,0 +1,13 @@
+package com.coincount.coin_count;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coincount/coin_count/CoinCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coincount.coin_count;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

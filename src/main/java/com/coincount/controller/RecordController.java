5b004021492d9ee88d2d563070f1380d9c68beb1
GDMontsho <montshogoitseone@gmail.com>
2025-03-31 13:00:07 +0200

    basic controller to handle API req

--- src/main/java/com/coincount/controller/RecordController.java
+++ src/main/java/com/coincount/controller/RecordController.java
@@ -4,6 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/records")
+
 public class RecordController {
+
+    @GetMapping
+    public String getAllRecords(){
+        return "Here are all the records";
+    }
     
 }
